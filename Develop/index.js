08f09c2e7bbc781093709ab7bd65c0dc6b93c8ec
PARKER GREENWELL <pgreenwell@alpine.local>
2022-11-14 11:37:25 -0700

    questions added to inquierer

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,45 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Enter the title of your project",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description of your project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter the installation instructions for your project",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter the usage information for your project",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter the contributions for your project",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter the test instructions for your project",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose the appropriate license for your project",
+    choices: ["Apache", "MIT", "GPL"],
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
